e4fd6c6614cef4016376c9a4c867176125b50a63
MikZarZSG <mikzarzsg@gmail.com>
2020-11-23 09:10:11 +0100

    Panel logowania

--- index.php
+++ index.php
@@ -2,9 +2,19 @@
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
-    <title>Słownik online</title>
+    <title>Słownik online - panel logowania</title>
 </head>
 <body>
+    <h1>Zaloguj się</h1>
     
+    <form action="login.php" method="post">
+        <div><label for="login">Login:</label></div>
+        <div><input type="text" name="login" id="login"></div>
+        
+        <div><label for="haslo">Hasło:</label></div>
+        <div><input type="password" name="haslo" id="haslo"></div>
+        
+        <div><input type="submit" value="Zaloguj się"></div>
+    </form>
 </body>
 </html>
