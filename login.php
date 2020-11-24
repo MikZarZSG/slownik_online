8e9222e64078640f740174fa7cc54f890a67bc25
MikZarZSG <mikzarzsg@gmail.com>
2020-11-24 13:12:50 +0100

    Mechanizm logowania
    - Zabezpieczenie przed nieprawidlowym zapytaniem
    - Komunikat w zaleznosci od wyniku zalogowania

--- login.php
+++ login.php
@@ -10,7 +10,7 @@
         
         //Błąd połączenia
         if($polaczenie->connect_error) {
-            throw new Exception ($polaczenie->connect_error);
+            throw new Exception($polaczenie->connect_error);
         }
         
         //Zapytanie SQL - pobranie użytkownika o podanych danych
@@ -21,9 +21,24 @@
             BINARY login = '$login' AND
             BINARY haslo = '$haslo'";
         
+        //Wykonanie zapytania SQL
         $wynik = $polaczenie->query($sql);
         
-        echo $wynik->num_rows;
+        //Czy zapytanie wykonało się poprawnie?
+        if($wynik) {
+            //Pomyślnie wykonano zapytanie
+            //Czy zalogowano?
+            if($wynik->num_rows > 0) {
+                //Pomyślnie zalogowano
+                echo "<p>Pomyślnie zalogowano</p>";
+            } else {
+                //Nieprawidłowa nazwa użytkownika lub hasło
+                echo "<p>Nieprawidłowa nazwa użytkownika lub hasło!</p>";
+            }
+        } else {
+            //Błąd w wykonaniu zapytania
+            throw new Exception("Niepoprawne zapytanie do bazy danych!");
+        }
         
         //Zamknięcie połączenia
         $polaczenie->close();
