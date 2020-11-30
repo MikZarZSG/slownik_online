c9b326c9957e8b9d6af6f79061e2b25430675393
MikZarZSG <mikzarzsg@gmail.com>
2020-11-30 12:34:23 +0100

    edit.php - przekierowania
    - Przekierowania w przypadku niezalogowania lub braku zmiennej

--- edit.php
+++ edit.php
@@ -1,3 +1,18 @@
 <?php
+    //Działanie zmiennych sesyjnych
+    session_start();
+
+    //Przekierowanie w przypadku, gdy użytkownik NIE jest zalogowany
+    if(!isset($_SESSION['czy_zalogowany']) || !$_SESSION['czy_zalogowany']) {
+        header('Location: index.php');
+        exit();
+    }
+    
+    //Przekierowanie w przypadku braku zmiennej GET
+    if(!isset($_GET['id'])) {
+        header('Location: panel.php');
+        exit();
+    }
+    
     echo $_GET['id'];
 ?>
